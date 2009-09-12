67e70122e3785ead47ff40f614441ebc9421573b
Nique <dominique@familiedegraaff.nl>
2009-09-13 00:41:30 +0200

    First Commit

---
+++ CityMania/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Mogre;
+using MogreFramework;
+
+namespace Test
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            OgreWindow win = new OgreWindow();
+
+            try
+            {
+                win.Go();
+            }
+            catch (System.Runtime.InteropServices.SEHException)
+            {
+                if (OgreException.IsThrown)
+                    MessageBox.Show(OgreException.LastException.FullDescription,
+                                    "An Ogre exception has occurred!");
+                else
+                    throw;
+            }
+        }
+    }
+}
